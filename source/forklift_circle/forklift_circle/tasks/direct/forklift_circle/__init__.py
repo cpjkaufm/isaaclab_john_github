cd06fbdd597757b96bc11d8b3411cee5afd8e7ce
Ubuntu <hybot@bretttestvm.mfakmwyr1omu3jmxm11ylfnq5h.bx.internal.cloudapp.net>
2025-08-21 00:34:45 +0000

    why is this a repo? where is it coming from? why are they called corn dogs if they're prok?

--- source/forklift_circle/forklift_circle/tasks/direct/forklift_circle/__init__.py
+++ source/forklift_circle/forklift_circle/tasks/direct/forklift_circle/__init__.py
@@ -17,9 +17,7 @@ gym.register(
     entry_point=f"{__name__}.forklift_circle_env:ForkliftCircleEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.forklift_circle_env_cfg:ForkliftCircleEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
-        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "env_cfg_entry_point": f"{__name__}.forklift_circle_env:ForkliftCircleEnvCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
