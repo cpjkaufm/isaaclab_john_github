7a9a45c41ff3d9a2532a90b27272540f61b50ad7
John <John.Kaufman@Hyster-Yale.com>
2025-08-25 11:14:33 -0700

    Add back wheels back

--- source/forklift_circle/forklift_circle/tasks/direct/forklift_circle/forklift_circle_env.py
+++ source/forklift_circle/forklift_circle/tasks/direct/forklift_circle/forklift_circle_env.py
@@ -40,9 +40,9 @@ class ForkliftCircleEnvCfg(DirectRLEnvCfg):
     # Register the joints to their respective functions
     throttle_dof_name = [
         "left_front_wheel_joint",
-        #"left_back_wheel_joint",
+        "left_back_wheel_joint",
         "right_front_wheel_joint",
-        #"right_back_wheel_joint",
+        "right_back_wheel_joint",
     ]
     steering_dof_name = [
         "left_rotator_joint",
